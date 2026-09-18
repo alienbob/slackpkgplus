308740f4540381eda06d3883905a8b6f93f205fa
Eric Hameleers <alienbob@vance.slackware.nl>
2026-09-18 22:51:55 +0200

    Bump the version of slackpkg+ to 1.8.3

--- src/slackpkgplus.sh
+++ src/slackpkgplus.sh
@@ -2057,7 +2057,7 @@ if [ "$SLACKPKGPLUS" = "on" ];then
     cleanup
   fi
 
-  SPKGPLUS_VERSION="1.8.2"
+  SPKGPLUS_VERSION="1.8.3"
   VERSION="$VERSION / slackpkg+ $SPKGPLUS_VERSION"
   
   if [ ${VERSION:0:4} == "2.82" ];then
