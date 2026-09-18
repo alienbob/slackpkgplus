af2f1daab60fd50142186604fb2f6d1e23366712
Eric Hameleers <alienbob@vance.slackware.nl>
2026-09-18 22:28:59 +0200

    Fix parsing bug which got exposed by slackpkg 16.0.0
    Contributed by nick8325 on LinuxQuestions.org,
    Slackpkgplus was counting the number of packages from the first search
    ("Looking for solibs in package list") and then using that as the total amount
    for the second search ("Looking for NEW packages to install").
    To make it recalculate the total you need to unset the TOPROCESS variable.

--- src/slackpkgplus.sh
+++ src/slackpkgplus.sh
@@ -133,6 +133,7 @@ if [ "$SLACKPKGPLUS" = "on" ];then
 
     # Patching makelist() original function to accept pkglist-pre
   eval "$(type makelist | sed -e $'1d;2c\\\nmakelist()\n' \
+                              -e '4iunset TOPROCESS' \
                               -e "/in package list/s/tr -d '\\\\\\\\'/tr -d '\\\\\\\\*'/" \
                               -e 's,cat ${WORKDIR}/pkglist > ${TMPDIR}/pkglist,cat $TMPDIR/pkglist-pre ${WORKDIR}/pkglist | applyblacklist > ${TMPDIR}/pkglist,' \
          )"
